04958d78afe3024142edffebfcb7ebd16703789e
Alex Godwin <alexkgodwin@gmail.com>
2019-01-17 16:05:11 -0500

    Added DO DNS API connection

--- connectors/digitalocean.go
+++ connectors/digitalocean.go
@@ -3,12 +3,18 @@ package connectors
 import (
 	"net"
 
+	"context"
+
+	"github.com/digitalocean/godo"
+	"golang.org/x/oauth2"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 // DigitalOceanDNSConnector implements the connector interface and provides methods
 // to interact with the DigitalOcean DNS API
 type DigitalOceanDNSConnector struct {
+	client *godo.Client
+	domain string
 }
 
 // Register this connector in the list
@@ -21,11 +27,29 @@ var _ Connector = (*DigitalOceanDNSConnector)(nil)
 
 // Setup is used to setup the command line details and connect to the parser
 func (do *DigitalOceanDNSConnector) Setup(app *kingpin.CmdClause) {
-	app.Flag("api_key", "The DigitalOcean API Key [Env: DO_API_KEY]").Envar("DO_API_KEY").String()
+	token := app.Flag("api_key", "The DigitalOcean API Key [Env: DO_API_KEY]").Envar("DO_API_KEY").Required().String()
+	tokenSource := &TokenSource{
+		AccessToken: *token,
+	}
+
+	oauthClient := oauth2.NewClient(context.Background(), tokenSource)
+	do.client = godo.NewClient(oauthClient)
+
+	do.domain = *app.Flag("domain", "The domain for on DigitalOcean DNS [Env: DO_DOMAIN]").Envar("DO_DOMAIN").Required().String()
+
 }
 
 // AddNode creates a new record for that node on the DO DNS API
 func (do *DigitalOceanDNSConnector) AddNode(address string, ip net.IP, ttl int) error {
+	recordAdd := &godo.DomainRecordEditRequest{
+		Type: "A",
+		Data: ip.String(),
+		Name: address,
+	}
+	ctx := context.TODO()
+	do.client.Domains.CreateRecord(ctx, do.domain, recordAdd)
+
+	// TODO: Return real error
 	return nil
 }
 
@@ -37,4 +61,17 @@ func (do *DigitalOceanDNSConnector) UpdateNode(address string, ip net.IP, ttl in
 // DeleteNode deletes the record for that node on the DO DNS API
 func (do *DigitalOceanDNSConnector) DeleteNode(address string) error {
 	return nil
+}
+
+// TokenSource is a type to store tokens for Oauth
+type TokenSource struct {
+	AccessToken string
+}
+
+// Token returns the Oauth token
+func (t *TokenSource) Token() (*oauth2.Token, error) {
+	token := &oauth2.Token{
+		AccessToken: t.AccessToken,
+	}
+	return token, nil
 }
--- go.mod
+++ go.mod
@@ -3,5 +3,10 @@ module github.com/gladiusio/gladius-dns-connector
 require (
 	github.com/alecthomas/template v0.0.0-20160405071501-a0175ee3bccc // indirect
 	github.com/alecthomas/units v0.0.0-20151022065526-2efee857e7cf // indirect
+	github.com/digitalocean/godo v1.7.3
+	github.com/google/go-querystring v1.0.0 // indirect
+	github.com/tent/http-link-go v0.0.0-20130702225549-ac974c61c2f9 // indirect
+	golang.org/x/net v0.0.0-20190110200230-915654e7eabc // indirect
+	golang.org/x/oauth2 v0.0.0-20190115181402-5dab4167f31c
 	gopkg.in/alecthomas/kingpin.v2 v2.2.6
 )
